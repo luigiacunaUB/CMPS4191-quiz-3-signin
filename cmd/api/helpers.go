34033f8522f46759771a0fa3b3154aeb321a5a92
Luigi Acuna <2015112896@ub.edu.bz>
2024-10-25 21:37:45 -0400

    added size limits

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 type envelope map[string]any
@@ -24,6 +28,55 @@ func (a *applicationDependencies) writeJSON(w http.ResponseWriter, status int, d
 	_, err = w.Write(jsResponse)
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func (a *applicationDependencies) readJSON(w http.ResponseWriter, r *http.Request, destination any) error {
+	//setting max size of request
+	maxBytes := 256_000
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	//checking for unknown fields
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	//start encoding
+	err := dec.Decode(destination)
+	if err != nil {
+		//check for the diffrent errors
+		var syntaxError *json.SyntaxError
+		var unmarshallTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("the body contains badly form-formed JSON (at charater %d)", syntaxError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("the body must not be empty")
+		case strings.HasPrefix(err.Error(), "json:unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json:unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("the body must not be larger than %d bytes", maxBytesError.Limit)
+		case errors.As(err, &unmarshallTypeError):
+			if unmarshallTypeError.Field != "" {
+				return fmt.Errorf("the body contains the incorrect JSON type for field &q", unmarshallTypeError.Field)
+			}
+			return fmt.Errorf("the body contains the incorrect JSON type (at charater &q)", unmarshallTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("the body must not be empty")
+
+		//the programmer messed up
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		//some other type of error
+		default:
+			return err
+		}
+	}
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("the body must only contain a single JSON value")
 	}
 	return nil
 }
