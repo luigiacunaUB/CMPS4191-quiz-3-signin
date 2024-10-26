5dd4ffc01a2e34bec441cca585a83d405cdafa51
Luigi Acuna <2015112896@ub.edu.bz>
2024-10-25 21:27:38 -0400

    added json limiters

--- cmd/api/signin.go
+++ cmd/api/signin.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -14,9 +13,9 @@ func (a *applicationDependencies) createSignInHandler(w http.ResponseWriter, r *
 	}
 
 	//decoding done here
-	err := json.NewDecoder(r.Body).Decode(&incomingData)
+	err := a.readJSON(w, r, &incomingData)
 	if err != nil {
-		a.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
+		a.badRequestResponse(w, r, err)
 		return
 	}
 
